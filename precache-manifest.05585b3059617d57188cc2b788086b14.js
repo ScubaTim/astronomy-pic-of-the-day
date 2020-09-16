edc66edb791ae0f539feaf09ad0f6699436f76bb
Tim Gentry <gentry.tim@gmail.com>
2020-09-16 15:07:56 -0400

    Updates

---
+++ precache-manifest.05585b3059617d57188cc2b788086b14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5100e6e43cc3e5d8159fa9560c12448",
+    "url": "/astronomy-pic-of-the-day/index.html"
+  },
+  {
+    "revision": "055476093c612dc8f081",
+    "url": "/astronomy-pic-of-the-day/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4623cdbc5c6296025812",
+    "url": "/astronomy-pic-of-the-day/static/css/main.aebec985.chunk.css"
+  },
+  {
+    "revision": "055476093c612dc8f081",
+    "url": "/astronomy-pic-of-the-day/static/js/2.3e11653e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/astronomy-pic-of-the-day/static/js/2.3e11653e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4623cdbc5c6296025812",
+    "url": "/astronomy-pic-of-the-day/static/js/main.7bf5a841.chunk.js"
+  },
+  {
+    "revision": "490e0b4f4e51e3a16a8a",
+    "url": "/astronomy-pic-of-the-day/static/js/runtime-main.7174b1c4.js"
+  }
+]);
