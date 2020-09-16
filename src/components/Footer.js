3462d30cc596ae99c67f90ef82ab41c66b569db1
Tim Gentry <gentry.tim@gmail.com>
2020-09-16 14:57:27 -0400

    footer styling changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Row, Col } from 'reactstrap';
 
 const Footer = ({ postDate }) => (
-    <div className="bg-dark text-light pt-3 pb-1 px-3">
-        <p>Astronomy Picture of the Day - <strong>{postDate}</strong></p>
-    </div>
+    <Row className="bg-dark text-light pt-3 pb-1 px-3">
+        <Col>
+            <p>Astronomy Picture of the Day ~ <strong>{postDate}</strong></p>
+        </Col>
+    </Row>
 );
 
 export default Footer;
